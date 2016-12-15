9d3fb120f884a9a9050923a68c0e91cc1827eac4
Alan Crosswell <alan@columbia.edu>
2016-12-15 21:30:08 +0000

    don't fail if the file is missing

--- facts.d/pingxmlfacts.py
+++ facts.d/pingxmlfacts.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # parse the XML for the configured JDBC connectors and create custom facts
 # Ignores the builtin hsqldb
+# TODO: replace this with JSON output from pf-admin-api instead.
 # TODO: make the pathname configurable.
 from xml.dom.minidom import parse
-dom1 = parse('/opt/pingfederate/server/default/data/pingfederate-jdbc-ds.xml')
+try:
+    dom1 = parse('/opt/pingfederate/server/default/data/pingfederate-jdbc-ds.xml')
+except:
+    exit(0)
 for ds in dom1.getElementsByTagName('datasources'):
     for tx in ds.getElementsByTagName('local-tx-datasource'):
         description = jndiname = url = None
